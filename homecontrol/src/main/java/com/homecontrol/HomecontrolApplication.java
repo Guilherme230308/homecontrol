7791f7316a1d524abf7ec6a61e063a209e618c37
Guilherme Carvalho <guilherme_230308@hotmail.com>
2019-01-28 21:47:13 -0200

    Criação do Projeto

---
+++ homecontrol/src/main/java/com/homecontrol/HomecontrolApplication.java
@@ -0,0 +1,14 @@
+package com.homecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecontrolApplication.class, args);
+	}
+
+}
+
---
+++ homecontrol/src/test/java/com/homecontrol/HomecontrolApplicationTests.java
@@ -0,0 +1,17 @@
+package com.homecontrol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomecontrolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
